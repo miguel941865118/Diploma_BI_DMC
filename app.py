259aac4e7ef0511e3dc031bd14e7dc7be2e3055e
miguel941865118 <miguel941865118@gmail.com>
2026-06-01 20:42:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title("Proyecto Final Diploma BI")
 
 st.sidebar.title("Parámetros")
 
-st.image("Python_logo.png,width = 500")
-st.sidebar.image("DMC.png,width = 100")
+st.image("Python_logo.png",width = 500)
+st.sidebar.image("DMC.png",width = 100)
 
 st.write("Elaborado por: Miguel Herrera")
