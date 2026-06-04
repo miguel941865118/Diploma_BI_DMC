9a717fcf719ccc98ca6c02e220fff3bb856b02aa
miguel941865118 <miguel941865118@gmail.com>
2026-06-03 20:04:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,14 @@ st.sidebar.image("DMC.png",width = 100)
 st.write("Elaborado por: Miguel Herrera")
 
 archivo = st.file_uploader("Cargue el archivo excel o csv")
+
+if archivo is not None :
+  if archivo.name.endwith(".csv"):
+    data = pd.read_csv(archivo)
+  elif archivo.name.endswith(".xlsx"):
+    data = pd.read_excel(archivo)
+  else:
+    st.write("Formato no válido")
+  
+else :
+  st.write("Por favor cargue su archivo")
